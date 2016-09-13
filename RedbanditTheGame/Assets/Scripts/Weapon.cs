eb97f0587dda0e1195e0b94e48c74608216133fc
RikLangeveld <rik_langeveld@hotmail.com>
2016-09-13 23:07:01 +0200

    Added shooting,

---
+++ RedbanditTheGame/Assets/Scripts/Weapon.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Weapon : MonoBehaviour {
+
+	public float fireRate = 0;
+	public float damage = 10;
+	public LayerMask whatToHit;
+    public Transform BulletTrailPrefab;
+    public Transform MuzzleFlashPrefab;
+
+    public AudioClip impact;
+    AudioSource audio;
+
+    float timeToSpawnEffect = 0;
+    public float effectSpawnRate = 10;
+
+    float timeToFire = 0;
+	Transform firePoint;
+
+	// Use this for initialization
+	void Start () {
+
+        firePoint = transform.FindChild("firePoint");
+
+        if (firePoint == null)
+        {
+			Debug.Log("Geen firepoint als child gevonden");
+        }
+
+        audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (fireRate == 0)
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                Shoot();
+            }
+        }
+        else
+        {
+            if (Input.GetButton("Fire1") && Time.time > timeToFire)
+            {
+                timeToFire = Time.time + 1 / fireRate;
+                Shoot();
+            }
+        }
+	}
+
+    void Shoot ()
+    {
+        // Zorgt ervoor dat de positie van de muis wordt omgezet naar de positie binnen de camera.
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firePoint.position.x, firePoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast (firePointPosition, mousePosition - firePointPosition, 100, whatToHit);
+
+        if (Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
+
+        Debug.DrawLine(firePointPosition, (mousePosition - firePointPosition)*100);
+        if (hit.collider != null)
+        {
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+            Debug.Log("We Hit " + hit.collider.name + " and did " + damage + " damage");
+        }
+    }
+
+    void Effect ()
+    {
+        audio.PlayOneShot(impact, 0.7f);
+
+        Instantiate (BulletTrailPrefab, firePoint.position, firePoint.rotation);
+
+        Transform clone = (Transform)Instantiate(MuzzleFlashPrefab, firePoint.position, firePoint.rotation);
+        clone.parent = firePoint;
+        float size = Random.Range(0.6f, 0.9f);
+        clone.localScale = new Vector3 (size, size, size);
+        Destroy(clone.gameObject, 0.02f);
+    }
+}
---
+++ RedbanditTheGame/Assets/Scripts/angleToMousePosition.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class angleToMousePosition : MonoBehaviour {
+
+
+	public float speed = 5f;
+
+	public float offSet = 0f;
+
+	void Update(){
+
+		// subtracting the position of the player from the mouse position (richtings vector bepalen)
+		Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+
+		difference.Normalize ();
+
+		// find the angle in degrees.
+		float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg; 
+		transform.rotation = Quaternion.Euler (0f, 0f, rotZ + offSet);
+	}
+}
