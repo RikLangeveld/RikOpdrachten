034c466f54c2fb02ea3e18c9d1d93b1e00ff9029
RikLangeveld <rik_langeveld@hotmail.com>
2016-09-15 23:55:30 +0200

    Added start music

---
+++ RedbanditTheGame/Assets/Scripts/MoveCamara.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCamara : MonoBehaviour {
+
+    public angleToMousePosition arm;
+    public GameObject GM;
+   
+
+	// Update is called once per frame
+	void FixedUpdate () {
+        if (transform.position.x < -173)
+        {
+            Debug.Log("check");
+            transform.Translate(1, 0, 0);
+        }
+        else
+        {
+            activateGame();
+        }
+
+    }
+
+    public void activateGame()
+    {
+        arm.GetComponent<angleToMousePosition>().enabled = true;
+        arm.GetComponent<Weapon>().enabled = true;
+        AudioSource audio = GM.GetComponent<AudioSource>();
+        audio.Stop();
+    
+    }
+}
