f1e2ee475cf5ac5207fb51d763e358260c256217
RikLangeveld <rik_langeveld@hotmail.com>
2017-11-22 22:31:52 +0100

    nh

--- Kungsleden/app/src/main/java/com/school/langevr004/kungsleden/MapFragment.java
+++ Kungsleden/app/src/main/java/com/school/langevr004/kungsleden/MapFragment.java
@@ -8,8 +8,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
 
 public class MapFragment extends Fragment {
+
+    private MapView mMap;
 
     public MapFragment() {
         // Required empty public constructor
@@ -19,6 +24,11 @@ public class MapFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+        //initialize streetmaps
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        //mMap = (MapView) v.findViewById(R.id.map);
+        //mMap.setTileSource(TileSourceFactory.MAPNIK);
+        //mMap.setMultiTouchControls(true);
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 }
