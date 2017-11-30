bb354f87753bd3a486fb39f692f622c2a371911a
RikLangeveld <rik_langeveld@hotmail.com>
2017-11-30 16:17:40 +0100

    dg

--- Kungsleden/app/src/main/java/com/school/langevr004/kungsleden/DescriptionFragment.java
+++ Kungsleden/app/src/main/java/com/school/langevr004/kungsleden/DescriptionFragment.java
@@ -7,8 +7,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-public class DescriptionFragment extends Fragment {
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerFragment;
+import com.google.android.youtube.player.YouTubePlayerSupportFragment;
+
+public class DescriptionFragment extends Fragment
+implements YouTubePlayer.OnInitializedListener{
+
+    public static final String DEVELOPER_KEY = "";
+    private static final String VIDEO_ID = "";
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    YouTubePlayerSupportFragment youTubePlayer;
 
     public DescriptionFragment() {
         // Required empty public constructor
@@ -23,7 +36,29 @@ public class DescriptionFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
+        View v = inflater.inflate(R.layout.fragment_description, container, false);
+        youTubePlayer = (YouTubePlayerSupportFragment) this.getChildFragmentManager().findFragmentById(R.id.youtubePlayerView);
+        youTubePlayer.initialize(DEVELOPER_KEY, this);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_description, container, false);
+        return v;
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b)
+    {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult)
+    {
+        if (youTubeInitializationResult.isUserRecoverableError()) {
+            youTubeInitializationResult.getErrorDialog(getActivity(), RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(
+                    "There was an error initializing the YouTubePlayer (%1$s)",
+                    youTubeInitializationResult.toString());
+            Toast.makeText(getActivity(), errorMessage, Toast.LENGTH_LONG).show();
+        }
     }
 }
